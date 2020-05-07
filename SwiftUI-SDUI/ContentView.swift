eb5d18505db7f980ebed0d8138570d7790758192
AnupPorter <anup.a@theporter.in>
2020-05-07 20:10:41 +0530

    Initial Commit

---
+++ SwiftUI-SDUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-SDUI
+//
+//  Created by porter on 07/05/20.
+//  Copyright © 2020 kinley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
